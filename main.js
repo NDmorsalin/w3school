b3d9332ef06a77f2c80736321d86401fbe23f73d
NDmorsalin <morsalin056@gmail.com>
2020-02-03 09:43:12 +0600

    check number is it even or not?

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 
-console.log("Hello World");
+function isEven(Ev){
+    if(Ev%2===0){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+    
+}
+var x=prompt("write your number");
+isEven(x);
+
+
